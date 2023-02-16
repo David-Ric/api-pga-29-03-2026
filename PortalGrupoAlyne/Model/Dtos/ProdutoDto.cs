5c9ec4014e4f6fc5b95948d4e8612786e7b68290
David-Ric <dric9669@gmail.com>
2023-02-16 13:08:54 -0300

    ajustes do cad prod

--- PortalGrupoAlyne/Model/Dtos/ProdutoDto.cs
+++ PortalGrupoAlyne/Model/Dtos/ProdutoDto.cs
@@ -10,7 +10,9 @@ namespace PortalGrupoAlyne.Model.Dtos
 
         [StringLength(60, ErrorMessage = "inserir no máximo 60 caracteres")]
         public string? Nome { get; set; }
+        [StringLength(4, ErrorMessage = "inserir no máximo 4 caracteres")]
         public string? TipoUnid { get; set; }
+        [StringLength(4, ErrorMessage = "inserir no máximo 4 caracteres")]
         public string? TipoUnid2 { get; set; }
         public int? Conv { get; set; }
 
--- PortalGrupoAlyne/Model/Produto.cs
+++ PortalGrupoAlyne/Model/Produto.cs
@@ -12,7 +12,9 @@ namespace PortalGrupoAlyne.Model
 
         [StringLength(60, ErrorMessage = "inserir no máximo 60 caracteres")]
         public string? Nome { get; set; }
+        [StringLength(4, ErrorMessage = "inserir no máximo 4 caracteres")]
         public string? TipoUnid { get; set; }
+        [StringLength(4, ErrorMessage = "inserir no máximo 4 caracteres")]
         public string? TipoUnid2 { get; set; }
         public int? Conv { get; set; }
 
