65263b0c3d38d3fd1ac17b607dfb8eba4e8bca5e
David-Ric <dric9669@gmail.com>
2023-05-25 19:34:35 -0300

    ajustes finais

--- PortalGrupoAlyne/Controllers/RestaurarMenuController.cs
+++ PortalGrupoAlyne/Controllers/RestaurarMenuController.cs
@@ -68,6 +68,7 @@ namespace PortalGrupoAlyne.Controllers
                 var novoLog = new List<Logs> {
                 new Logs { Id = 1, VersaoApi = "1.0.0", AtualizadoEm = DateTime.Now },
                  new Logs { Id = 3, VersaoApi = "1.0.2", AtualizadoEm = DateTime.Now },
+                 new Logs { Id = 3, VersaoApi = "1.0.4", AtualizadoEm = DateTime.Now },
                 };
                 var logsNoBanco = await _context.Logs.ToListAsync();
                 foreach (var log in novoLog)
