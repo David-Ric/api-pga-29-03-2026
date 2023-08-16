df24de1915c4f4cf2eb50892af4ee9078dd88741
Cigel-Dev <ti@grupoalyne.com.br>
2023-08-16 09:39:34 -0300

    ajuste v12

--- PortalGrupoAlyne/Controllers/RestaurarMenuController.cs
+++ PortalGrupoAlyne/Controllers/RestaurarMenuController.cs
@@ -315,7 +315,8 @@ WHERE
                  new Logs { Id = 8, VersaoApi = "1.1.005", AtualizadoEm = DateTime.Now },
                  new Logs { Id = 9, VersaoApi = "1.1.009", AtualizadoEm = DateTime.Now },
                  new Logs { Id = 10, VersaoApi = "1.1.010", AtualizadoEm = DateTime.Now },
-                  new Logs { Id = 10, VersaoApi = "1.1.011", AtualizadoEm = DateTime.Now },
+                  new Logs { Id = 11, VersaoApi = "1.1.011", AtualizadoEm = DateTime.Now },
+                   new Logs { Id = 12, VersaoApi = "1.1.012", AtualizadoEm = DateTime.Now },
                 };
                 var logsNoBanco = await _context.Logs.ToListAsync();
                 foreach (var log in novoLog)
